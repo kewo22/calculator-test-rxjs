82d2e4fabce8e7d2f07c05b1dee9e845f0cad888
kewo22 <kewinf271@gmail.com>
2020-01-08 14:38:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# calculator-test-rxjs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/calculator-test-rxjs)
---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  
+  onNumClick(num:string) {
+    const currNum: number = +num;
+    (document.querySelector('#num') as HTMLInputElement).value += currNum.toString();
+  }
+
+  onOperationClick(op:string) {
+    
+    (document.querySelector('#ans') as HTMLInputElement).value += (document.querySelector('#num') as HTMLInputElement).value;
+  }
+}
